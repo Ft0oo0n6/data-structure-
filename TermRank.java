d454797d0583d92de0a028649dafbfb0c975ccd8
maha-mj <444200646@student.ksu.edu.sa>
2024-11-23 16:40:14 +0300

    Update TermRank.java

--- TermRank.java
+++ TermRank.java
@@ -47,7 +47,7 @@ public class TermRank {
     @Override
     public String toString() {
         String documents = "";
-        int x = 0, i = 0; // Initialize counters for the loop
+        int x = 0, i = 0; 
 
         while (x < documentId.length) {
             if (i == 0 && documentId[x] != 0) {
@@ -57,72 +57,10 @@ public class TermRank {
                 documents += ", " + String.valueOf(x);
                 i++;
             }
-            x++; // Increment the counter
+            x++; 
         }
 
         return word + "[" + documents + ']';
     }
 }
-/*
-public class TermRank {
-    Vocabulary word;
-    int  [] documentId ;
 
-    public TermRank() {
-        docIDS = new int [50];
-        for (int i = 0 ; i < docIDS.length ; i++)
-            docIDS [i] = 0;
-        word = new Vocabulary("");
-    }
-
-    public TermRank(String word, int [] docIDS) {
-        this.word = new Vocabulary(word);
-        this.docIDS = new int [docIDS.length];
-        for (int i = 0 ; i < docIDS.length ; i++)
-            this.docIDS [i] = docIDS[i];
-
-    }
-    
-    public void add_docID ( int docID)
-    {
-        this.docIDS[docID] ++;
-    }
-
-    public void setVocabulary(Vocabulary word)
-    {
-        this. word = word; 
-    }
-    
-    public Vocabulary getVocabulary()
-    {
-         return word;
-    }
-    
-    public int [] getDocs ()
-    {
-        int [] test = new int [docIDS.length];
-        for ( int i = 0 ; i < test.length ; i++)
-            test[i] = docIDS[i];
-        return test;
-    }
-    
-    @Override
-    public String toString() {
-        String docs = "";
-        for (int i = 0, j = 0 ; i < docIDS.length; i++)
-            if ( j == 0 && docIDS [i] != 0 )
-            {
-                docs += " " + String.valueOf(i) ;
-                j++;
-            }
-            else if ( docIDS [i] != 0 )
-            {
-                docs += ", " + String.valueOf(i) ;
-                j++;
-            }
-        
-        return word + "[" + docs + ']';
-    }
-    
-    
-}*/
