d72a481cc4e31abe21417799b0f96e3cedc16b88
maha-mj <444200646@student.ksu.edu.sa>
2024-11-23 03:55:27 +0300

    Update TermRank.java

--- TermRank.java
+++ TermRank.java
@@ -1,7 +1,72 @@
-
 public class TermRank {
     Vocabulary word;
-    int  [] docIDS ;
+    int[] documentId;
+
+    public TermRank() {
+        documentId = new int[50];
+        int i = 0;
+        while (i < documentId.length) {
+            documentId[i] = 0;
+            i++;
+        }
+        word = new Vocabulary("");
+    }
+
+    public TermRank(String word, int[] documentId) {
+        this.word = new Vocabulary(word);
+        this.documentId = new int[documentId.length];
+        int i = 0;
+        while (i < documentId.length) {
+            this.documentId[i] = documentId[i];
+            i++;
+        }
+    }
+
+    public void addDocumentId(int docId) {
+        this.documentId[docId]++;
+    }
+
+    public void setVocab(Vocabulary word) {
+        this.word = word;
+    }
+
+    public Vocabulary getVocab() {
+        return word;
+    }
+
+    public int[] printAllDoc() {
+        int[] check = new int[documentId.length];
+        int i = 0;
+        while (i < check.length) {
+            check[i] = documentId[i];
+            i++;
+        }
+        return check;
+    }
+
+    @Override
+    public String toString() {
+        String documents = "";
+        int x = 0, i = 0; // Initialize counters for the loop
+
+        while (x < documentId.length) {
+            if (i == 0 && documentId[x] != 0) {
+                documents += " " + String.valueOf(x);
+                i++;
+            } else if (documentId[x] != 0) {
+                documents += ", " + String.valueOf(x);
+                i++;
+            }
+            x++; // Increment the counter
+        }
+
+        return word + "[" + documents + ']';
+    }
+}
+/*
+public class TermRank {
+    Vocabulary word;
+    int  [] documentId ;
 
     public TermRank() {
         docIDS = new int [50];
@@ -57,7 +122,7 @@ public class TermRank {
             }
         
         return word + "[" + docs + ']';
-    }
-    
-    
-}
+    }
+    
+    
+}*/
